68f76191c38638915b3650b8078bfc940472078d
Jason Baldridge <jasonbaldridge@gmail.com>
2013-04-13 14:10:39 -0500

    Started moving toward Breeze style definitions for features, observations, etc.

--- src/main/scala/nak/core/Classifier.scala
+++ src/main/scala/nak/core/Classifier.scala
@@ -1,9 +1,15 @@
 package nak.core
 
-trait Classifier extends (Array[(Int,Double)] => Array[Double])
+import nak.liblinear.{Model => LiblinearModel, FeatureNode, LiblinearConfig, Linear, Problem, Parameter}
 
-trait FeatureMap[F] {
-  def indexOfFeature(feature: F): Option[Int]
+trait FeatureMap {
+  def indexOfFeature(feature: String): Option[Int]
+  def indexFeatureObservation(fobs: FeatureObservation): Option[(Int,Double)] = {
+    indexOfFeature(fobs.feature) match {
+      case Some(index) => Some(index, fobs.magnitude)
+      case None => None
+    }
+  }
 }
 
 trait LabelMap[L] {
@@ -12,28 +18,39 @@ trait LabelMap[L] {
   def numLabels: Int
 }
 
-case class AttrVal(val attr: String, val value: String) {
-  override def toString = attr + "=" + value
+
+trait Observation[+T] {
+  def features: T
 }
 
-trait IndexedClassifier[L,F] extends Classifier with LabelMap[L] with FeatureMap[F]
+trait Example[+L,+T] extends Observation[T] {
+  def label: L
+}                                                       
+
+case class StandardExample(label: String, features: Array[FeatureObservation]) 
+extends Example[String,Array[FeatureObservation]]
+
+case class FeatureObservation(feature: String, magnitude: Double = 1.0)
+
+trait Classifier extends (Array[(Int,Double)] => Array[Double])
+
+trait IndexedClassifier[L,F] extends Classifier with LabelMap[L] with FeatureMap
 
 trait StringIndexedClassifier extends IndexedClassifier[String,String]
 
 trait LinearModelAdaptor extends nak.core.LinearModel with StringIndexedClassifier {
 
-  def eval(observations: Array[(String,Double)]) = {
-    val indexedValues = observations.flatMap { case(c,v) => indexOfFeature(c) match {
-      case Some(index) => Some((index, v))
-      case None => None
-    }}
-    apply(indexedValues)
+  def eval(observations: Array[FeatureObservation]) =
+    apply(observations.flatMap(indexFeatureObservation))
+
+  def eval(context: Array[String], values: Array[Float]): Array[Double] = {
+    val fobservations = for ((f,m) <- context.zip(values)) yield
+      FeatureObservation(f,m.toDouble)
+    eval(fobservations)
   }
-
-  def eval(context: Array[String], values: Array[Float]): Array[Double] = 
-    eval(context.zip(values.map(_.toDouble)))
-
-  def eval(context: Array[String]) = eval(context.zip(Stream.continually(1.0)))
+  
+  def eval(context: Array[String]) = 
+    eval(context.map(descr => FeatureObservation(descr)))
     
   def getOutcome(i: Int) = labelOfIndex(i)
   def getIndex(outcome: String) = indexOfLabel(outcome)
@@ -43,7 +60,6 @@ trait LinearModelAdaptor extends nak.core.LinearModel with StringIndexedClassifi
 
 object ClassifierUtil {
 
-  import nak.core.LinearModel
   import java.text.DecimalFormat
 
   /**
@@ -89,5 +105,143 @@ object ClassifierUtil {
       }
   }
 
+
+
+}
+
+
+/**
+ * A wrapper to Liblinear that conforms to the LinearModel interface.
+ * 
+ * @author jasonbaldridge
+ */
+class LiblinearClassifier(
+  val model: LiblinearModel, 
+  val lmap: Map[String, Int], 
+  val fmap: Map[String, Int]) extends LinearModelAdaptor {
+
+  import nak.liblinear.{Feature => LiblinearFeature}
+
+  // Assumes labels are indexed 0 to n in label map
+  val labels = lmap.toSeq.sortBy(_._2).unzip._1.toArray
+
+  def indexOfFeature(feature: String) = fmap.get(feature)
+  def indexOfLabel(label: String) = lmap(label)
+  def labelOfIndex(index: Int) = labels(index)
+  val numLabels = labels.length
+
+  val getModelType = nak.core.AbstractModel.ModelType.Liblinear
+
+  def apply(context: Array[(Int,Double)]): Array[Double] =
+    predict(context.map(c=>new FeatureNode(c._1,c._2).asInstanceOf[LiblinearFeature]))
+
+  def predict(context: Array[LiblinearFeature]): Array[Double] = {
+    val labelScores = Array.fill(numLabels)(0.0)
+    Linear.predictProbability(model, context, labelScores)
+    labelScores
+  }
+}
+
+object LiblinearClassifier {
+  
+  def apply(model: LiblinearModel, labels: Array[String], features: Array[String]) =
+    new LiblinearClassifier(model, labels.zipWithIndex.toMap, features.zipWithIndex.toMap)
+
+}
+
+/**
+ * Train a Liblinear classifier from data.
+ * 
+ * @author jasonbaldridge
+ */
+class LiblinearTrainer(config: LiblinearConfig) {
+
+  import nak.liblinear.{Feature => LiblinearFeature}
+  import LiblinearTrainer._
+
+  if (!config.showDebug) Linear.disableDebugOutput
+
+  def apply(
+    responses: Seq[Double],
+    observations: Seq[Seq[(Int,Double)]], 
+    numFeatures: Int
+  ): LiblinearModel =
+    apply(responses.toArray, createLiblinearMatrix(observations), numFeatures)
+
+
+  def apply(
+    responses: Array[Double],
+    observations: Array[Array[LiblinearFeature]],
+    numFeatures: Int
+  ): LiblinearModel = {
+
+    val problem = new Problem
+    problem.y = responses
+    problem.x = observations
+    problem.l = responses.length
+    problem.n = numFeatures
+
+    // Can make the solver type a parameter if want to use other solvers in LibLinear.
+    val param = new Parameter(config.solverType, config.cost, config.eps)
+    Linear.train(problem, param)
+  }
+
+}
+
+/**
+ * A helper object
+ */
+object LiblinearTrainer {
+
+  import nak.liblinear.{Feature => LiblinearFeature}
+
+  def train(indexer: nak.data.DataIndexer, config: LiblinearConfig = new LiblinearConfig()) = {
+
+    val labels = indexer.getOutcomeLabels
+    
+    // We unfortunately need to fix up the contexts so that feature indices start at 1.
+    val zeroBasedFeatures = indexer.getPredLabels
+    val zeroReindex = zeroBasedFeatures.length
+    val features = zeroBasedFeatures ++ Array(zeroBasedFeatures(0))
+    features(0) = "DUMMY FEATURE"
+    
+    val contexts = indexer.getContexts.map { context => {
+      context.map { _ match {
+        case 0 => zeroReindex
+        case x => x
+      }}
+    }}
+
+    // Use values of 1.0 if the features were binary.
+    val values = 
+      if (indexer.getValues != null) indexer.getValues
+      else contexts.map(_.map(x=>1.0f))
+
+    // Get the responses and observations ready for Liblinear
+    val responses = indexer.getOutcomeList.map(_.toDouble)
+    val observationsAsTuples = contexts.zip(values).map{ 
+      case (c,v) => 
+        c.zip(v).groupBy(_._1).mapValues(_.map(_._2)).mapValues(_.sum.toFloat).toArray
+    }
+    
+    val observations = createLiblinearMatrix(observationsAsTuples)
+
+    // Train the model, and then return the classifier.
+    val model = new LiblinearTrainer(config)(responses, observations, features.length)
+
+    LiblinearClassifier(model, labels, features);
+  }
+
+  def createLiblinearMatrix(observations: Seq[Seq[(Int,Double)]]): Array[Array[LiblinearFeature]] =  
+    observations.map { features =>
+      features.map{ case(a,v) => new FeatureNode(a,v).asInstanceOf[LiblinearFeature] }.toArray
+    }.toArray
+
+  def createLiblinearMatrix(observations: Array[Array[(Int,Float)]]): Array[Array[LiblinearFeature]] =  
+    observations.map { features => {
+      features
+        .sortBy(_._1)
+        .map{ case(a,v) => new FeatureNode(a,v).asInstanceOf[LiblinearFeature] }
+    }}
 
 }

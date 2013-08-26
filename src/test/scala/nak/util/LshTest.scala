ef19f304589f06ef5490ce83aeddf2cd8222b222
Jason Baldridge <jasonbaldridge@gmail.com>
2013-08-26 17:32:27 -0500

    Fixes #27: Update test to reflect changes in LSH code.

--- src/test/scala/nak/util/LshTest.scala
+++ src/test/scala/nak/util/LshTest.scala
@@ -12,22 +12,17 @@ class LocalitySensitiveHashSpec extends FunSpec {
       "/data/twitter/example_tweets.txt")
 
     it ("should index and find near matches") {
-      val lines = io.Source.fromInputStream(tweets).getLines
-        .map(StringCleaner.onlyAlpha)
-        .toIndexedSeq
+      val lines = io.Source.fromInputStream(tweets).getLines.toIndexedSeq
       
       // Hash all all documents read from file
-      val lsh = new LocalitySensitiveHash(
-        lines, shingleLength=3, numRows=100, numBands=10)
+      val lsh = new LocalitySensitiveHash(lines, shingleLength=4)
       
       // find the documents that are most similar to the below string
       val testString = "RT @Adam_Schefter:QB Tebow broke the combined record for QBs with a 38-inch vertical jump. He also ran an impressive 40 time "
       
       val matchingIds = lsh.findSimilar(testString).toList.sorted
-      println("** " + matchingIds)
-      
-      // Need to deal better with randomness
-      assert((matchingIds == List(34,193,851)) | (matchingIds == List(851)))
+
+      assert(matchingIds === List(34,193,851))
     }
   }
 

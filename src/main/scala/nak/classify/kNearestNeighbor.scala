51fc9e8da6bdee234b1469de9959715184707f2c
Gabriel Schubiner <gabeos@cs.washington.edu>
2014-06-13 10:52:33 -0700

    Updated kNN to use Beam instead of BoundedPriorityQueue. Removed BPQ

--- src/main/scala/nak/classify/kNearestNeighbor.scala
+++ src/main/scala/nak/classify/kNearestNeighbor.scala
@@ -1,37 +1,13 @@
-/*
- * dialogue -- The University of Washington Dialogue Framework
- *
- * Copyright 2013 - Gabriel Schubiner
- *
- * kNearestNeighbor.scala is part of dialogue.
- *
- * dialogue is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * dialogue is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with dialogue.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package nak.classify
 
 import nak.data.Example
-import breeze.linalg.Counter
 import breeze.generic.UFunc.UImpl2
-import scala.collection.mutable
-import nak.util.BoundedPriorityQueue
 import breeze.storage.DefaultArrayValue
-import nak.space.dm.euclidean
 import breeze.linalg._
+import breeze.collection.mutable.Beam
 
 /**
- * dialogue
+ * kNearestNeighbor
  * 6/8/14
  * @author Gabriel Schubiner <gabeos@cs.washington.edu>
  *
@@ -51,7 +27,7 @@ class kNearestNeighbor[L, T,D](c: Iterable[Example[L, T]],
     *       nearest neighbor set, where prob is inversely prop. to distance.to nearest label.
     */
   override def scores(o: T): Counter[L, Double] = {
-    val pq = BoundedPriorityQueue[(L, Double)](k)(Ordering.by((_: (_, Double))._2))
+    val pq = Beam[(L, Double)](k)(Ordering.by((_: (_, Double))._2))
     pq ++ c.map(e => (e.label, dm(e.features, o)))
     implicit val default = DefaultArrayValue[Double](Double.PositiveInfinity)
     pq.foldLeft(Counter[L, Double]())((minAcc: Counter[L, Double], next: (L, Double)) =>

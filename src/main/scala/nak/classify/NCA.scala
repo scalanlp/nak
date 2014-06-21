f827c1aaa4b9cf01209cda16e564c40568135322
Gabriel Schubiner <gabeos@cs.washington.edu>
2014-06-20 18:11:32 -0700

    Added NCA shell.

--- src/main/scala/nak/classify/NCA.scala
+++ src/main/scala/nak/classify/NCA.scala
@@ -21,6 +21,12 @@
 
 package nak.classify
 
+import breeze.linalg.{sum, DenseMatrix, Counter}
+import breeze.optimize.FirstOrderMinimizer.OptParams
+import breeze.optimize.StochasticDiffFunction
+import breeze.util.Isomorphism
+import nak.data.Example
+
 /**
  * dialogue
  * 6/19/14
@@ -28,10 +34,34 @@ package nak.classify
  *
  *
  */
-class NCA {
-
+class NCA[L,T](examples: Iterable[Example[L,T]], A: DenseMatrix[Double]) extends Classifier[L,T] {
+  /** For the observation, return the score for each label that has a nonzero
+    * score.
+    */
+  override def scores(o: T): Counter[L, Double] = ???
 }
 
 object NCA {
-  class Trainer[L,T](k: Int = 1)
+
+  class Trainer[L,T](opt: OptParams = OptParams()) extends Classifier.Trainer[L,T] {
+    override type MyClassifier = NCA[L,T]
+
+    override def train(data: Iterable[Example[L, T]]): MyClassifier = {
+
+      implicit val exampleVectorIsomorphism = Isomorphism[Example[L,T],T]((t: Example[L,T]) => t.features, (u: T) => new Example[L,T] {
+        override def id: String = ""
+
+        override def label: L = ???
+
+        override def features: T = ???
+      })
+
+      val f = new StochasticDiffFunction[DenseMatrix[Double]] {
+        override def calculate(x: T): (Double, T) = {
+          //Expected number of points correctly classified
+          val value = sum(data.filter(_.label == x))
+        }
+      }
+    }
+  }
 }

30726972661c46e256064652483c41dfa7649c4f
Gabriel Schubiner <gabeos@cs.washington.edu>
2014-06-20 18:34:59 -0700

    Added AveragedPerceptronTrainer. Same as PerceptronTrainer, but with weights averaging.

--- src/main/scala/nak/classify/Perceptron.scala
+++ src/main/scala/nak/classify/Perceptron.scala
@@ -2,7 +2,7 @@ package nak.classify
 
 import nak.data.Example
 import breeze.math.MutableInnerProductSpace
-import breeze.linalg.{SparseVector, Counter}
+import breeze.linalg.{argmax, SparseVector, Counter}
 import breeze.util.Index
 import scala.reflect.ClassTag
 
@@ -22,13 +22,44 @@ object Perceptron {
         val l: L = ex.label
         val feats = ex.features
         val ctr = result.scores(feats)
-        if(ctr.size == 0 || ctr.argmax != l) {
+        if(ctr.size == 0 || argmax(ctr) != l) {
           weights(l) += feats
           if(ctr.size != 0) {
-            weights(ctr.argmax) -= feats
+            weights(argmax(ctr)) -= feats
           }
         }
       }
+      result
+    }
+  }
+
+  class AveragedTrainer[L,T](maxPasses: Int = 20)(
+    implicit vspace: MutableInnerProductSpace[T, Double],
+    man: ClassTag[T]) extends Classifier.Trainer[L,T] {
+    import vspace._
+    type MyClassifier = LinearClassifier[L,UnindexedLFMatrix[L,T],Counter[L,Double],T]
+    def train(data: Iterable[Example[L,T]]) = {
+      val labelIndex = Index(data.map(_.label))
+      val weights = new LFMatrix[L,T](zeros(data.head.features), labelIndex)
+      val avgWeights = new LFMatrix[L,T](zeros(data.head.features),labelIndex)
+      weights(data.head.label); // seed with one label
+      avgWeights(data.head.label); // seed with one label
+      import LFMatrix._
+      val result:MyClassifier = new LinearClassifier[L, UnindexedLFMatrix[L, T], Counter[L, Double], T](weights.unindexed,Counter[L,Double]())
+      for( i <- 0 until maxPasses;  ex <- data) {
+        val l: L = ex.label
+        val feats = ex.features
+        val ctr = result.scores(feats)
+        if(ctr.size == 0 || argmax(ctr) != l) {
+          weights(l) += feats
+          avgWeights(l) += (feats :* i.toDouble)
+          if(ctr.size != 0) {
+            weights(argmax(ctr)) -= feats
+            avgWeights(argmax(ctr)) -= (feats :* i.toDouble)
+          }
+        }
+      }
+      weights -= (avgWeights :* (1.0/maxPasses))
       result
     }
   }
